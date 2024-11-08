bae8ab8f13c70b3ff369f7be9a8e79164a6c55cc
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2024-11-08 10:56:22 -0300

    feat: criei uma função para alternar as sombras de acordo com o amontoado de lixo

--- Assets/Scripts/Misc/TrashDump.cs
+++ Assets/Scripts/Misc/TrashDump.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class TrashDump : MonoBehaviour, IInteractable
 {
-    [Range(0, 12)]
+    [Range(0, 6)]
     [SerializeField] private int trashBagsAmount = 0;
 
     [SerializeField] private float trashSpawnTimer = 1f;    
     
     private bool isCoroutineReady = false;
 
-    public List<Sprite> sprites;
+    public List<Sprite> trasgBagsSprites;
+    public List<GameObject> trashBagsShadows;
 
     private SpriteRenderer spriteRenderer;    
 
@@ -38,12 +39,14 @@ public class TrashDump : MonoBehaviour, IInteractable
     void Update()
     {
 
-        if (sprites.Count != 0 && trashBagsAmount < sprites.Count && trashBagsAmount >= 0)
+        if (trasgBagsSprites.Count != 0 && trashBagsAmount < trasgBagsSprites.Count && trashBagsAmount >= 0)
         {
-            spriteRenderer.sprite = sprites[trashBagsAmount];
+            spriteRenderer.sprite = trasgBagsSprites[trashBagsAmount];            
         }
 
-        if (sprites.Count != 0 && trashBagsAmount >= sprites.Count) isAccumulated = true;
+        AlternateTrashBagsShadowsWithTheAmount();
+
+        if (trasgBagsSprites.Count != 0 && trashBagsAmount >= trasgBagsSprites.Count) isAccumulated = true;
         else isAccumulated = false;
 
         if (isAccumulated && !isCoroutineReady) StartCoroutine(SpawnTrashInTheZoneRoutine());
@@ -91,7 +94,75 @@ public class TrashDump : MonoBehaviour, IInteractable
             Random.Range(zoneBounds.min.x, zoneBounds.max.x),
             Random.Range(zoneBounds.min.y, zoneBounds.max.y)
         );
-    }    
+    }  
+    
+    private void AlternateTrashBagsShadowsWithTheAmount()
+    {
+        switch (trashBagsAmount)
+        {
+            case 0:
+                foreach (var item in trashBagsShadows)
+                {
+                    item.SetActive(false);
+                }
+                break;
+
+            case 1:
+                trashBagsShadows[0].SetActive(true);
+                trashBagsShadows[1].SetActive(false);
+                trashBagsShadows[2].SetActive(false);
+                trashBagsShadows[3].SetActive(false);
+                trashBagsShadows[4].SetActive(false);
+                trashBagsShadows[5].SetActive(false);
+                break;
+
+            case 2:
+
+                trashBagsShadows[0].SetActive(true);
+                trashBagsShadows[1].SetActive(true);
+                trashBagsShadows[2].SetActive(false);
+                trashBagsShadows[3].SetActive(false);
+                trashBagsShadows[4].SetActive(false);
+                trashBagsShadows[5].SetActive(false);
+                break;
+
+            case 3:
+                trashBagsShadows[0].SetActive(true);
+                trashBagsShadows[1].SetActive(true);
+                trashBagsShadows[2].SetActive(true);
+                trashBagsShadows[3].SetActive(false);
+                trashBagsShadows[4].SetActive(false);
+                trashBagsShadows[5].SetActive(false);
+                break;
+
+            case 4:
+                trashBagsShadows[0].SetActive(true);
+                trashBagsShadows[1].SetActive(true);
+                trashBagsShadows[2].SetActive(true);
+                trashBagsShadows[3].SetActive(true);
+                trashBagsShadows[4].SetActive(false);
+                trashBagsShadows[5].SetActive(false);
+                break;
+
+            case 5:
+                trashBagsShadows[0].SetActive(true);
+                trashBagsShadows[1].SetActive(true);
+                trashBagsShadows[2].SetActive(true);
+                trashBagsShadows[3].SetActive(true);
+                trashBagsShadows[4].SetActive(true);
+                trashBagsShadows[5].SetActive(false);
+                break;
+
+            case 6:
+                trashBagsShadows[0].SetActive(true);
+                trashBagsShadows[1].SetActive(true);
+                trashBagsShadows[2].SetActive(true);
+                trashBagsShadows[3].SetActive(true);
+                trashBagsShadows[4].SetActive(true);
+                trashBagsShadows[5].SetActive(true);
+                break;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
