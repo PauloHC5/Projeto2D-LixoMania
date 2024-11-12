ea2cbc8f6326450ac730680c26b40e85428437b5
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2024-11-12 15:24:06 -0300

    fix: arrumei o problema do collider do saco de lixo não colidindo com a lixeira

--- Assets/Scripts/Misc/TrashBag.cs
+++ Assets/Scripts/Misc/TrashBag.cs
@@ -44,9 +44,9 @@ public class TrashBag : MonoBehaviour, IThrowingObject, IInteractable
         }
 
         return null;        
-    }    
+    }        
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "TrashCan" && !canInteract)
         {
