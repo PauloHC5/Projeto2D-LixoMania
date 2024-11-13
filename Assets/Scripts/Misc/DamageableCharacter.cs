a427c412ae815761d34ff850df6df659812ad25c
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2024-11-13 15:33:47 -0300

    refactor: essa classe tera agora apenas a responsabilidade de aplicar o knockback e servir como molde para as classes filhas

--- Assets/Scripts/Misc/DamageableCharacter.cs
+++ Assets/Scripts/Misc/DamageableCharacter.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class DamageableCharacter : MonoBehaviour, IDamageable
 {
-    [SerializeField] protected float invencibilityTime = 0.25f;
-    [SerializeField] protected bool invencible = false;
+    [SerializeField] protected float health = 5;    
 
     protected int takeDamage = Animator.StringToHash("takeDamage");
 
@@ -13,57 +12,27 @@ public class DamageableCharacter : MonoBehaviour, IDamageable
     protected Rigidbody2D rb;
     protected Collider2D col;
 
-    protected IEnumerator invencibleRoutine;
-
-    public bool Invencible
-    {
-        get => invencible;
-
-        set
-        {
-            invencible = value;
-        }
-    }
-
-
-    private NPC npc;
+    protected IEnumerator invencibleRoutine;        
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         if(animator == null) animator = GetComponentInChildren<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-        npc = GetComponent<NPC>();
+        rb = GetComponent<Rigidbody2D>();        
         col = GetComponent<Collider2D>();
     }           
 
     public virtual void OnHit(float damage, Vector2 knockback)
     {
-        if (!invencible)
-        {            
-            //Apply the force to the slime
-            rb.AddForce(knockback, ForceMode2D.Impulse);
-
-            Invencible = true;            
-            StartCoroutine(InvencibleRoutine());            
-
-            if (animator) animator.SetTrigger(takeDamage);
-            if (npc != null) npc.StunNPC();
-
-            AudioManager.Instance.PlaySFX(AudioManager.Instance.takeDamage);
-        }
+        //Apply the force to the slime
+        rb.AddForce(knockback, ForceMode2D.Impulse);
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.takeDamage);        
     }
 
     public virtual void OnHit(float damage)
     {
-        
-    }
-
-    private IEnumerator InvencibleRoutine()
-    {        
-        col.excludeLayers |= (1 << LayerMask.NameToLayer("Enemies"));
-        yield return new WaitForSeconds(invencibilityTime);
-        col.excludeLayers &= ~(1 << LayerMask.NameToLayer("Enemies"));
-        invencible = false;
-    }    
+        /*
+         * This method will ber overwriten by the child classes
+         */
+    }     
 }
