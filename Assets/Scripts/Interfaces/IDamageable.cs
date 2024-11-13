26a7e5cf3a0c2446b9d6a08df914b39dadbaef05
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2024-11-13 15:32:33 -0300

    refactor: tirei a obrigatoriedade de implementar um atributo invencible

--- Assets/Scripts/Interfaces/IDamageable.cs
+++ Assets/Scripts/Interfaces/IDamageable.cs
@@ -1,9 +1,7 @@
 ﻿using UnityEngine;
 
 public interface IDamageable {
-    
-    public bool Invencible { get; set; }
-
+        
     void OnHit(float damage, Vector2 knockback);
     void OnHit(float damage);
 }
