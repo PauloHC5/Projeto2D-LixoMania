e18aca9922299fd3418c5167ff9b47631f1306aa
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2024-11-23 20:22:11 -0300

    feat: tutorial manager implementado

--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -2,18 +2,34 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {    
     public GameObject boss;
-    public TextMeshProUGUI bossTextMeshPro;
-    public Image cellphone;
+    public TextMeshProUGUI bossTextMeshPro;    
+
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private BossDictionary bossDictionary;
+
+
+    [Header("Listeners")]
+    public GameEventListener playerHealthEventListener;
+
+    public GameEventListener playerTrashDumpAccumulatedEventListener;
+
+    private Dictionary<string, string> bossTexts;
+
+    private bool breakRoutine = false;
 
     private void Start()
     {
         GameManager.OnGameStateChanged += OnGameChanged;
+
+        bossTexts = bossDictionary.ToDictionary();
     }
 
     private void OnDestroy()
@@ -30,17 +46,99 @@ public class TutorialManager : MonoBehaviour
     }
 
     private IEnumerator IntroductionRoutine()
-    {
-        yield return new WaitForSeconds(1f);
+    {        
+        yield return new WaitForSeconds(1f);        
         AudioManager.Instance.PlaySFX(AudioManager.Instance.telefone);
-        yield return new WaitForSeconds(2f);
-        AudioManager.Instance.StopSFX();
+        yield return new WaitForSeconds(1.5f);
+        AudioManager.Instance.StopSFX();        
         AudioManager.Instance.PlaySFX(AudioManager.Instance.telefonePickup);
         yield return new WaitForSeconds(1.5f);
         boss.gameObject.SetActive(true);
+        bossTextMeshPro.text = bossTexts["Introduction pt1"];
         AudioManager.Instance.PlaySFX(AudioManager.Instance.cartoonTalking);
-        yield return new WaitForSeconds(15f);
+        yield return new WaitForSeconds(5f);
+        bossTextMeshPro.text = bossTexts["Introduction pt2"];
+        yield return new WaitForSeconds(7f);
+        bossTextMeshPro.text = bossTexts["Introduction pt3"];
+        yield return new WaitForSeconds(7f);
+        bossTextMeshPro.text = bossTexts["Introduction pt4"];
+        yield return new WaitForSeconds(2f);
+        AudioManager.Instance.StopSFX();
+        yield return new WaitForSeconds(5f);        
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.telefonePickup);        
+        boss.gameObject.SetActive(false);
+    }
+
+    public void HealthTutorial()
+    {
+        if(!breakRoutine)
+        {
+            StartCoroutine(TutorialRotine("Heal"));
+            StartCoroutine(TutorialBreakRoutine());
+            playerHealthEventListener.enabled = false;
+        }        
+    }    
+
+    public void TrashDumpTutorial()
+    {
+        if (!breakRoutine)
+        {
+            StartCoroutine(TutorialRotine("TrashDump"));
+            StartCoroutine(TutorialBreakRoutine());
+            playerTrashDumpAccumulatedEventListener.enabled = false;
+        }
+    }      
+
+    public void PolutionTutorial()
+    {
+        if (!breakRoutine)
+        {
+            StartCoroutine(TutorialRotine("Polution"));
+            StartCoroutine(TutorialBreakRoutine());
+            playerTrashDumpAccumulatedEventListener.enabled = false;
+        }
+    }
+
+    private IEnumerator TutorialRotine(string tutorialSubject)
+    {
+        boss.gameObject.SetActive(true);
+        bossTextMeshPro.text = bossTexts[tutorialSubject];
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.cartoonTalking);
+        yield return new WaitForSeconds(10f);
         AudioManager.Instance.StopSFX();
         AudioManager.Instance.PlaySFX(AudioManager.Instance.telefonePickup);
+        boss.gameObject.SetActive(false);
     }
+
+    private IEnumerator TutorialBreakRoutine()
+    {
+        breakRoutine = true;
+        yield return new WaitForSeconds(30f);
+        breakRoutine = false;
+    }
+}
+
+[Serializable]
+public class BossDictionary
+{
+    [SerializeField] BossDictionaryItem[] bossDictionaryItems;
+
+    public Dictionary<string, string> ToDictionary()
+    {
+        Dictionary<string, string> newDict = new Dictionary<string, string>();
+
+        foreach(var item in bossDictionaryItems)
+        {
+            newDict.Add(item.subject, item.text);
+        }
+
+        return newDict;
+    }
+}
+
+[Serializable]
+public class BossDictionaryItem
+{
+    [SerializeField] public string subject;
+    [SerializeField] public string text;
 }
